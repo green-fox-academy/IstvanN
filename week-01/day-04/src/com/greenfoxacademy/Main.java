a4d868fc54c8ddbbf075ab08529259c376af6fc2
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-02 14:01:34 +0200

    comitting 2 java files

---
+++ week-01/day-04/src/com/greenfoxacademy/Main.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int myNumb = 10;
+    if (myNumb < 10) {
+      System.out.println("number is fewer");
+    } else if (myNumb == 10) {
+      System.out.println("number is 10");
+    } else {
+      System.out.println("number is greater");
+    }
+    }
+  }
