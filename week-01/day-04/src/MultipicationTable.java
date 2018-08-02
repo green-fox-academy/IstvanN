e9a1d1f7b9c529373508e1bdeaccebb46b7b75ce
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-02 18:42:10 +0200

    created 4 more java exercises

---
+++ week-01/day-04/src/MultipicationTable.java
@@ -0,0 +1,5 @@
+public class MultipicationTable {
+  public static void main(String[] args) {
+    
+  }
+}
--- week-01/day-04/src/PartyIndicator.java
+++ week-01/day-04/src/PartyIndicator.java
@@ -6,6 +6,24 @@ public class PartyIndicator {
 
 //    Asking for girls
     System.out.println("How many girls?");
-    int 
+    int girlsNumber = scanner.nextInt();
+
+//    Asking for boys
+    System.out.println("How many boys?");
+    int boysNumber = scanner.nextInt();
+
+//    Total amount
+    int total = girlsNumber + boysNumber;
+
+//    The if comes in
+    if (girlsNumber == 0) {
+      System.out.println("Sausage party.");
+    } else if (total >= 20 && girlsNumber == boysNumber) {
+      System.out.println("The Party is excellent!");
+    } else if (total >= 20 && girlsNumber != boysNumber) {
+      System.out.println("Quite cool party!");
+    } else {
+      System.out.println("Average party...");
+    }
   }
 }
