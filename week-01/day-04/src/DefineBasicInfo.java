278110fc6b56c4b6fe723d6439a72a2263d3c517
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-02 16:01:02 +0200

    casual comitting

---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,14 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+
+//  Defining the variables
+    String myName = "Istvan Nemeth";
+    int myAge = 29;
+    double myHeight = 1.78;
+    boolean ifMarried = false;
+
+//    Now printing them
+    System.out.println("Hey guys, my name's " + myName + " and I'm " + myAge + " years old. Besides I'm " + myHeight + "m tall and if you heard I was married well the information was definitely " + ifMarried + ".");
+
+  }
+}
---
+++ week-01/day-04/src/Swap.java
@@ -0,0 +1,18 @@
+public class Swap {
+  public static void main(String[] args) {
+//    This is the original setup
+    int a = 123;
+    int b = 526;
+
+    System.out.println(a);
+    System.out.println(b);
+
+//    Now for the swap
+    a = 526;
+    b = 123;
+
+    System.out.println(a);
+    System.out.println(b);
+
+  }
+}
