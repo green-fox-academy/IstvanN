768811ec1c785a72b1b4625d1d071db80759ef6f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-02 14:37:27 +0200

    comitting the first like 3 or 4 exercises of java

---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,8 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+//    Greeting my classmates be like
+    System.out.println("Hello Nits!");
+    System.out.println("Hiya Tamas!");
+    System.out.println("'sup C?");
+  }
+}
---
+++ week-01/day-04/src/IntroduceYourself.java
@@ -0,0 +1,15 @@
+public class IntroduceYourself {
+  public static void main(String[] args) {
+//     Creating the variables
+
+    String myName = "Istvan Nemeth";
+    int myAge = 29;
+    double myHeight = 1.78;
+
+//     Printing out the variables
+
+    System.out.println("My name is " + myName + ".");
+    System.out.println("I am " + myAge + " years old.");
+    System.out.println("I am " + myHeight + " meters tall.");
+  }
+}
