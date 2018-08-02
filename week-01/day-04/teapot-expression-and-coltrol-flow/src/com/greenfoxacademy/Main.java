f3de9e6e5876b75d35010a446a54e67d99865cac
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-02 12:48:07 +0200

    Fist editings with IntelliJ

---
+++ week-01/day-04/teapot-expression-and-coltrol-flow/src/com/greenfoxacademy/Main.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy;
+
+public class Main {
+
+  public static void main(String[] args) {
+//    its just a comment
+    int myNumb = 10;
+    if (myNumb < 10) {
+      System.out.println("fewer than 10");
+    } else if (myNumb == 10) {
+      System.out.println("equals 10");
+    } else {
+      System.out.println("greater than 10");
+    }
+  }
+}
