81aec65825e7c6d557cf261eba3cfcb97a49e0ca
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-04 12:25:06 +0200

    added parametric

--- week-01/day-05/src/ParametricAverage.java
+++ week-01/day-05/src/ParametricAverage.java
@@ -5,13 +5,19 @@ public class ParametricAverage {
 //    Asking for the number
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Please enter a number:");
+    System.out.println("Please enter a number:"); // Asking for number
     int userNumb = scanner.nextInt();
 
 //    Asking for the integers
+    int sum = 0;
+
     for (int i = 0; i < userNumb; i++) {
-      System.out.println("Please enter an integer");
+      System.out.println("Please enter an integer:");
       int userInt = scanner.nextInt();
+      sum += userInt;
     }
+
+    double average = (double) (sum) / userNumb;
+    System.out.println("Sum: " + sum + " Average: " + average);
   }
 }
