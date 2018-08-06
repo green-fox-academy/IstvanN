bdb1bf79855c3c57cd489a4a85f898ddd118bccb
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-06 13:05:03 +0200

    adding Doubling

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,15 @@
+public class Doubling {
+  public static void main(String[] args) {
+
+//    Creating the 'baseNum' variable
+    int baseNum = 123;
+
+//    Printing the result of 'doubling(baseNum)'
+    System.out.println(doubling(baseNum));
+  }
+
+  //    This function takes an integer and doubles it
+  public static int doubling(int numberToDouble) {
+    return numberToDouble * 2;
+  }
+}
