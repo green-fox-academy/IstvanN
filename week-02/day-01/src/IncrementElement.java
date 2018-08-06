2df11fe2d9dd32935ed7a2370b913fb79292f2f1
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-06 15:25:28 +0200

    adding IncrementElement and PrintAll

---
+++ week-02/day-01/src/IncrementElement.java
@@ -0,0 +1,15 @@
+public class IncrementElement {
+  public static void main(String[] args) {
+
+//    Creating the array
+    int[] t = {1, 2, 3, 4, 5};
+
+//    Incrementing the third element
+    t[2]++;
+
+//    Printing the third element
+    System.out.println(t[2]);
+
+
+  }
+}
---
+++ week-02/day-01/src/PrintAll.java
@@ -0,0 +1,12 @@
+public class PrintAll {
+  public static void main(String[] args) {
+
+//    Creating the array
+    int[] numbers = {4, 5, 6, 7};
+
+//    Printing the numbers
+    for (int number: numbers) {
+      System.out.print(number + " ");
+    }
+  }
+}
