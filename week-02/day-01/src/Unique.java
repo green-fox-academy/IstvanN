a76346f448cd978e9bd2ab197a6432c1d3f8cd40
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-06 23:48:37 +0200

    updating Unique

--- week-02/day-01/src/Unique.java
+++ week-02/day-01/src/Unique.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 public class Unique {
   public static void main(String[] args) {
 
+//    Printing to try out if it works
     System.out.println(removeDuplicates(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
 
   }
 
 
+//  This function takes a list of numbers as parameters, and removes the duplicates
   public static ArrayList removeDuplicates(int[] originalList) {
     ArrayList<Integer> uniqueList = new ArrayList();
     for (int number : originalList) {
