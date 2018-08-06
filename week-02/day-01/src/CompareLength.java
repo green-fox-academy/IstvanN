8ea80e88aa77f9e2960cf5aa2f828fc4e56252d6
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-06 14:35:23 +0200

    adding CompareLength and Third

---
+++ week-02/day-01/src/CompareLength.java
@@ -0,0 +1,13 @@
+public class CompareLength {
+  public static void main(String[] args) {
+
+//    Creating the two arrays
+    int[] p1 = {1, 2, 3};
+    int[] p2 = {4, 5};
+
+//    Printing if p2 has more elements
+    if(p2.length > p1.length) {
+      System.out.println("P2 has more elements."); // It won't print since the condition is false
+    }
+  }
+}
---
+++ week-02/day-01/src/Third.java
@@ -0,0 +1,10 @@
+public class Third {
+  public static void main(String[] args) {
+
+//    Creating the array
+    int[] q = {4, 5, 6, 7};
+
+//    Printing the third element
+    System.out.println(q[2]);
+  }
+}
