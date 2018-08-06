f993c1d089e588b5e93bda9b4ea5633e2863a25d
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-06 14:58:37 +0200

    readding ChangeElement

--- week-02/day-01/src/ChangeElement.java
+++ week-02/day-01/src/ChangeElement.java
@@ -1,17 +1,11 @@
 public class ChangeElement {
   public static void main(String[] args) {
 
-//      Creating the array
+//    Creating the array
     int[] s = {1, 2, 3, 8, 5, 6};
+    s[3] = 4;
 
-//  Changing the forth element to '4'
-    for (int element : s) {
-      if (element == s[3]) {
-        s[3] = 4;
-      }
-    }
-
-//    Printing out the fourth element of the array
+//    Printing the fourth element
     System.out.println(s[3]);
   }
 }
