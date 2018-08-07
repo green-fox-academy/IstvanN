725d6cae2e4b3e99f621959919d9f7f1460a8a65
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-07 11:52:22 +0200

    adding SimpleReplace

---
+++ week-02/day-02/src/SimpleReplace.java
@@ -0,0 +1,12 @@
+public class SimpleReplace {
+  public static void main(String[] args) {
+
+//    Creating the variable
+    String example = "In a dishwater far, far away";
+
+//    Replacing the 'dishwater' to 'galaxy'
+    example = example.replace("dishwater", "galaxy");
+
+    System.out.println(example);
+  }
+}
