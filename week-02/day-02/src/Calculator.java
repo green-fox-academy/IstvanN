d7666317f100b44153c0467b59b2cbf5949fc355
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-07 19:03:34 +0200

    adding Calculator

---
+++ week-02/day-02/src/Calculator.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class Calculator {
+  public static void main(String[] args) {
+
+//    Setting up the scanner
+    Scanner scanner = new Scanner(System.in);
+
+//    Greeting and asking for input
+    System.out.println("Greetings. Please type in the expression:");
+    String userInput = scanner.nextLine();
+
+//    Converting the string userInput into an array
+    String[] userInputArray = userInput.split(" ");
+
+    String operation = userInputArray[0];
+    Double operationOne = Double.parseDouble(userInputArray[1]);
+    Double operationTwo = Double.parseDouble(userInputArray[2]);
+
+//    Calling the function
+    calculate(operation, operationOne, operationTwo);
+  }
+
+  //  This is the calculate method
+  public static double calculate(String operation, Double numberOne, Double numberTwo) {
+    double result = 0;
+
+    if (operation.equals("+")) {
+      result = numberOne + numberTwo;
+    } else if (operation.equals("-")) {
+      result = numberOne - numberTwo;
+    } else if (operation.equals("*")) {
+      result = numberOne * numberTwo;
+    } else if (operation.equals("/")) {
+      result = numberOne / numberTwo;
+    } else if (operation.equals("%")) {
+      result = numberOne % numberTwo;
+    }
+
+//    System.out.println(result);
+    System.out.println(result);
+    return result;
+  }
+}
+
