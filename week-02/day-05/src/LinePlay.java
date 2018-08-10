6836555cc858d6b3ed83e7907ec6ddfe7ed9cc44
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-10 11:32:52 +0200

    added Lineplay

---
+++ week-02/day-05/src/LinePlay.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+  public static void mainDraw(Graphics graphics) {
+
+//    The purple part
+    graphics.setColor(Color.magenta);
+
+    for (int i = 30; i < WIDTH; i += 20) {
+      graphics.drawLine(i, 5, WIDTH, i);
+    }
+
+    graphics.setColor(Color.green);
+
+    for (int i = 30; i < HEIGHT; i += 20) {
+      graphics.drawLine(5, i, i, HEIGHT);
+    }
+
+
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+
+}
