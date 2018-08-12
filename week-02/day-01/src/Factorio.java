6ba4c2c8c80b5c4daaa7ab5e033747c47b543f27
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-12 21:55:12 +0200

    updating Factorio

--- week-02/day-01/src/Factorio.java
+++ week-02/day-01/src/Factorio.java
@@ -2,7 +2,7 @@ public class Factorio {
   public static void main(String[] args) {
 
 //    Trying out the function
-    System.out.println(factorio(10));
+    System.out.println(factorio(5));
   }
 
   //  This function returns the input's factorial
