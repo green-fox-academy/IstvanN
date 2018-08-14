3eae717048bb3ef93373fb947cb1e6445e600f1d
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-14 13:05:07 +0200

    added PrintEachLine and my-file.txt for experiencing around

--- week-03/day-02/src/PrintEachLine.java
+++ week-03/day-02/src/PrintEachLine.java
@@ -7,10 +7,11 @@ import java.util.List;
 public class PrintEachLine {
   public static void main(String[] args) {
 
-    Path path = Paths.get("my-file.txt");
+    Path path = Paths.get("/home/archiez/greenfox/IstvanN/week-03/day-02/files/my-file.txt");
 
     try {
       List lines = Files.readAllLines(path);
+      System.out.println(lines);
     } catch (IOException ex) {
       System.out.println("Unable to read file: " + path);
     }
