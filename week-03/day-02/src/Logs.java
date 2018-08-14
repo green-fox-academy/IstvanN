05e35d90a849e4485098fbb61778c4ad77bd20b3
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-14 15:25:18 +0200

    updated Logs with the basics of the function

--- week-03/day-02/src/Logs.java
+++ week-03/day-02/src/Logs.java
@@ -1,18 +1,31 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Logs {
   public static void main(String[] args) {
-//    Setting up the path
-    Path path = Paths.get("files/log.txt");
 
-//    Adding the content to an arraylist
+  }
+
+  public static ArrayList getIP(String filename) {
+
+//    Setting up the path of the method
+    Path path = Paths.get("files/" + filename);
+//    Setting up the content arraylist
     try {
       List<String> content = Files.readAllLines(path);
     } catch (Exception ex) {
-      System.out.println("There was a problem with the following file: " + path);
+      System.out.println("Couldn't read the file: " + filename);
     }
+
+//    Setting up an empty list for the IP addresses
+    ArrayList<String> listOfIP = new ArrayList<>();
+
+//    Adding the IP addresses to the empty list
+    
+
+    return listOfIP;
   }
 }
