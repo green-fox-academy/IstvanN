000cad7076cb03897c75eb4764f31999602d6d99
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-14 14:30:26 +0200

    added WriteMultipleLines and updated my-file

---
+++ week-03/day-02/src/WriteMultipleLines.java
@@ -0,0 +1,31 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class WriteMultipleLines {
+  public static void main(String[] args) {
+    writeToFile("files/my-file.txt", "Paranoid Android", 4);
+  }
+
+  public static void writeToFile(String filePath, String word, int numberOfLines) {
+
+//    I setup the path
+    Path path = Paths.get(filePath);
+
+//    Adding the 'word' 'numberOfLines' times to the arraylist
+    ArrayList<String> lines = new ArrayList<>();
+    for (int i = 0; i < numberOfLines; i++) {
+      lines.add(word);
+    }
+
+//    Try and catch if error
+    try {
+      Files.write(path, lines);
+    } catch (Exception ex) {
+      System.out.println("Couldn't write to the file: " + path);
+    }
+
+  }
+}
