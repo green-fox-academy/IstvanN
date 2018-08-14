b4577b6d33604eb2ae7dad46f4d64fb55c3dcd72
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-14 13:41:44 +0200

    added WriteSingleLine and overwrote the my-file.txt

---
+++ week-03/day-02/src/WriteSingleLine.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class WriteSingleLine {
+  public static void main(String[] args) {
+
+    Path path = Paths.get("/home/archiez/greenfox/IstvanN/week-03/day-02/files/my-file.txt");
+    ArrayList<String> myName = new ArrayList<>(Arrays.asList("Istvan Nemeth"));
+
+    try {
+      Files.write(path, myName);
+    } catch (IOException ex) {
+      System.out.println("Unable to write file: " + path);
+    }
+  }
+}
