e8ab9bdc10103a30cae892a7c7ef68149136443a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-15 17:23:59 +0200

    updated PetrolStation with tested main

--- week-03/day-03/petrolstation/src/Main.java
+++ week-03/day-03/petrolstation/src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
   public static void main(String[] args) {
+    Car trabant = new Car();
+    Station petrolStation = new Station();
+    getStatus(petrolStation, trabant);
+    petrolStation.refill(trabant);
+    getStatus(petrolStation, trabant);
+
+    Car opel = new Car();
+    petrolStation.refill(opel);
+    getStatus(petrolStation, opel);
 
   }
 
   public static void getStatus(Station station, Car car) {
-    System.out.println("The car's fuel: " + car.gasAmount);
     System.out.println("The Station's remaining fuel: " + station.gasAmount);
+    System.out.println("The car's fuel: " + car.gasAmount);
   }
 }
