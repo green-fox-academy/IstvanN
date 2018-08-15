ca147f2a64b5080a27c6d96d66932eb90f84ef76
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-15 13:57:10 +0200

    added Domino, seems to work

---
+++ week-03/day-03/dominoes/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+    List<Domino> dominoes = initializeDominoes();
+    // You have the list of Dominoes
+    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // eg: [2, 4], [4, 3], [3, 5] ...
+
+    List<Domino> sortedDominoes = new ArrayList<>();
+    sortedDominoes.add(dominoes.get(0));
+
+    int counter = 0;
+    while (sortedDominoes.size() != dominoes.size()) {
+      for (int i = 1; i < dominoes.size(); i++) {
+        if (sortedDominoes.get(counter).getValues()[1] == dominoes.get(i).getValues()[0]) {
+          sortedDominoes.add(dominoes.get(i));
+          counter++;
+        }
+      }
+    }
+
+    System.out.println(sortedDominoes);
+  }
+
+  static List<Domino> initializeDominoes() {
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+    return dominoes;
+  }
+}
