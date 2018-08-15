0dde477b51f83d3f9a4ac702e9ee7655711ee7d3
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-15 14:05:47 +0200

    updated Domino main

--- week-03/day-03/dominoes/src/Main.java
+++ week-03/day-03/dominoes/src/Main.java
@@ -5,10 +5,7 @@ public class Main {
 
   public static void main(String[] args) {
     List<Domino> dominoes = initializeDominoes();
-    // You have the list of Dominoes
-    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
-    // eg: [2, 4], [4, 3], [3, 5] ...
-
+    
     List<Domino> sortedDominoes = new ArrayList<>();
     sortedDominoes.add(dominoes.get(0));
 
