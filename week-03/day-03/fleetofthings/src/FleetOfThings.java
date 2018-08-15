97c285d7642179840624323aa88810adef9713fb
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-15 14:59:05 +0200

    updated FleetOfThings main

--- week-03/day-03/fleetofthings/src/FleetOfThings.java
+++ week-03/day-03/fleetofthings/src/FleetOfThings.java
@@ -7,7 +7,21 @@ public class FleetOfThings {
     // 2. [ ] Remove the obstacles
     // 3. [x] Stand up
     // 4. [x] Eat lunch
-    
+
+    Thing milk = new Thing("Get milk");
+    fleet.add(milk);
+
+    Thing obstacles = new Thing("Remove the obstacles");
+    fleet.add(obstacles);
+
+    Thing standUp = new Thing("Stand up");
+    standUp.complete();
+    fleet.add(standUp);
+
+    Thing eatLunch = new Thing("Eat lunch");
+    eatLunch.complete();
+    fleet.add(eatLunch);
+
     System.out.println(fleet);
   }
 }
