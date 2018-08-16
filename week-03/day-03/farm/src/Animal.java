c5b10dab71a8d70736a6e89a5714f77887174c6c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-16 21:37:44 +0200

    added Farm with Animal class and Farm class

---
+++ week-03/day-03/farm/src/Animal.java
@@ -0,0 +1,30 @@
+public class Animal {
+
+  int hunger;
+  int thirst;
+
+  public Animal() {
+    hunger = 50;
+    thirst = 50;
+
+  }
+
+  public int eat() {
+    hunger -= 1;
+    return hunger;
+  }
+
+  public int drink() {
+    thirst -= 1;
+    return thirst;
+  }
+
+  public void play() {
+    hunger += 1;
+    thirst += 1;
+  }
+
+  public void printStatus() {
+    System.out.println("This animal has " + hunger + " hunger and has " + thirst + " thirst.");
+  }
+}
---
+++ week-03/day-03/farm/src/Farm.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Farm {
+  List<Animal> animals;
+  int slots;
+
+  public Farm(int slots) {
+    this.slots = slots;
+  }
+
+  public List addAnimal(Animal animal) {
+    animals.add(animal);
+    return animals;
+  }
+
+  public List breed(Animal animal) {
+    animal = new Animal();
+    animals.add(animal);
+    return animals;
+  }
+
+  public List slaughter(Animal animal) {
+    animals.remove(animal);
+    return animals;
+  }
+}
