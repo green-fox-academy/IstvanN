fbd9158a5736386a34c87afa5e0aaf22bbffdd9b
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-16 10:44:07 +0200

    updated Counter

--- week-03/day-04/src/Counter.java
+++ week-03/day-04/src/Counter.java
@@ -5,8 +5,8 @@ public class Counter {
 
   public static int countDown(int number) {
     if (number == 0) {
-      System.out.println(0);
-      return 0;
+      System.out.println(number);
+      return number;
     } else {
       System.out.println(number);
       return countDown(number - 1);
