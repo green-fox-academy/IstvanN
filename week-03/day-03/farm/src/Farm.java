e3d9007d0e1d789a8c29e586cdb9f6303d5672d8
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-16 21:41:55 +0200

    updated the functions of Farm class

--- week-03/day-03/farm/src/Farm.java
+++ week-03/day-03/farm/src/Farm.java
@@ -10,17 +10,22 @@ public class Farm {
 
   public List addAnimal(Animal animal) {
     animals.add(animal);
+    slots--;
     return animals;
   }
 
   public List breed(Animal animal) {
     animal = new Animal();
     animals.add(animal);
+    slots--;
     return animals;
   }
 
   public List slaughter(Animal animal) {
     animals.remove(animal);
+
+
+    slots++;
     return animals;
   }
 }
--- week-03/day-03/farm/src/Main.java
+++ week-03/day-03/farm/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // write your code here
+  }
+
+  public static List getStatus (Animal animal, Farm farm) {
+    System.out.println("Number of animals in the farm: " );
+  }
 }
