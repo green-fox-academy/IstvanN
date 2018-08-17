64ba3c49267c7089fefe11dd1effc473c7477c8f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-17 11:57:09 +0200

    PVP testing started

--- week-03/day-05/PVP/src/Barbarian.java
+++ week-03/day-05/PVP/src/Barbarian.java
@@ -37,10 +37,9 @@ public class Barbarian {
     System.out.println(name + " used Battle Cry, and healed " + heal + " points. HP left: " + healthPoint);
     return healthPoint;
   }
-
   //    The weapons
   Weapon sword = new Weapon(5, true, false);
-  Weapon axe = new Weapon(5, true, true);
+  Weapon axe = new Weapon(5, false, true);
   Weapon hammer = new Weapon(10, true, true);
 
   public void useWeapon(Barbarian barbarian, Mage mage) {
@@ -49,14 +48,17 @@ public class Barbarian {
       barbarian.healthPoint += sword.selfHeal;
       barbarian.healthPoint -= sword.selfDamage;
       mage.healthPoint -= sword.weaponDamage + barbarian.baseDamage;
+      System.out.println(barbarian.name + " used his Enchanted Sword, and caused " + sword.weaponDamage + barbarian.baseDamage + " damage!");
     } else if (whichWeapon == 2) {
       barbarian.healthPoint += axe.selfHeal;
       barbarian.healthPoint -= axe.selfDamage;
       mage.healthPoint -= axe.weaponDamage + barbarian.baseDamage;
+      System.out.println(barbarian.name + " used his Cursed Axe, and caused " + axe.weaponDamage + barbarian.baseDamage + " damage!");
     } else {
       barbarian.healthPoint += hammer.selfHeal;
       barbarian.healthPoint -= hammer.selfDamage;
       mage.healthPoint -= hammer.weaponDamage + barbarian.baseDamage;
+      System.out.println(barbarian.name + " used his Hammer of Absolute Destruction, and caused " + hammer.weaponDamage + barbarian.baseDamage + " damage!");
     }
   }
 }
--- week-03/day-05/PVP/src/Weapon.java
+++ week-03/day-05/PVP/src/Weapon.java
@@ -2,7 +2,7 @@ public class Weapon {
   int weaponDamage;
   int selfHeal;
   int selfDamage;
-  boolean enchanted;
+
 
   public Weapon(int weaponDamage, boolean enchanted, boolean cursed) {
     this.weaponDamage = weaponDamage;
@@ -12,13 +12,7 @@ public class Weapon {
     }
     if (cursed) {
       this.weaponDamage += 5;
-      selfDamage = 5;
+      selfDamage = 10;
     }
   }
-
-//  public void use(Barbarian barbarian, Mage mage) {
-//    barbarian.healthPoint += selfHeal;
-//    barbarian.healthPoint -= selfDamage;
-//    mage.healthPoint -= weaponDamage + barbarian.baseDamage;
-//  }
 }
