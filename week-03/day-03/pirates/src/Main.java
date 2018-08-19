85bbce54509a977042f32b7bd45f0c9b08974506
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-19 22:16:41 +0200

    updated Ship with party and battle methods, init battleApp main

--- week-03/day-03/pirates/src/Main.java
+++ week-03/day-03/pirates/src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    Pirate pirate1 = new Pirate();
-    Pirate pirate2 = new Pirate();
-
-    pirate1.brawl(pirate2);
-    pirate1.getStatus();
-    pirate2.getStatus();
-
-    pirate1.drinkSomeRum();
+    Ship pirateShip = new Ship("Black Pearl");
+    pirateShip.fillShip("Jack Sparrow");
+    pirateShip.shipStatus();
   }
 }
--- week-03/day-03/pirates/src/Ship.java
+++ week-03/day-03/pirates/src/Ship.java
@@ -5,17 +5,19 @@ import java.util.Random;
 public class Ship {
   List<Pirate> ship;
   String name;
+  int score;
 
   public Ship(String name) {
     this.name = name;
     ship = new ArrayList<>();
+    score = numberOfAliveCrew() + captainStatus();
   }
 
-  public List fillShip() {
+  public List fillShip(String captainName) {
     Random random = new Random();
-    for (int i = 0; i < random.nextInt(10) + 1; i++) {
-      if (i == 0) {
-        ship.add(new Pirate("Captain Sparrow"));
+    for (int i = 0; i <= random.nextInt(10); i++) {
+      if (i == random.nextInt(10)) {
+        ship.add(new Pirate(captainName));
       } else {
         ship.add(new Pirate());
       }
@@ -23,18 +25,21 @@ public class Ship {
     return ship;
   }
 
-  public void captainStatus() {
+  public int captainStatus() {
+    int captainRumLevel = 0;
     for (Pirate pirate : ship) {
       if (pirate.isCaptain) {
         if (pirate.alive) {
           System.out.println(pirate.captainName + " has the following stats:");
           System.out.println("Rum level: " + pirate.rumLevel);
           System.out.println("Passed out: " + pirate.passedOut);
+          captainRumLevel += pirate.rumLevel;
         } else {
           System.out.println("The captain is dead. RUN!!!!");
         }
       }
     }
+    return captainRumLevel;
   }
 
   public int numberOfAliveCrew() {
@@ -51,5 +56,31 @@ public class Ship {
     System.out.println(name + " is in the following condition:");
     captainStatus();
     System.out.println("The number of alive pirates are: " + numberOfAliveCrew());
+  }
+
+  public List party() {
+    Random random = new Random();
+    for (Pirate pirate : ship) {
+      for (int i = 0; i <= random.nextInt(10); i++)
+        pirate.drinkSomeRum();
+    }
+    return ship;
+  }
+
+  public boolean battle(Ship otherShip) {
+    Random random = new Random();
+    if (score > otherShip.score) {
+      for (int i = 0; i <= random.nextInt(10); i++) {
+        otherShip.ship.get(i).die();
+      }
+      party();
+      return true;
+    } else {
+      for (int i = 0; i <= random.nextInt(10); i++) {
+        ship.get(i).die();
+      }
+      otherShip.party();
+      return false;
+    }
   }
 }
