1249c73d5a7a28ed834e1f9a3e965bd2545e2638
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-19 16:24:26 +0200

    update Pirate class with die()

--- week-03/day-03/pirates/src/Pirate.java
+++ week-03/day-03/pirates/src/Pirate.java
@@ -1,15 +1,58 @@
+import java.util.Random;
+
 public class Pirate {
   int rumLevel;
   boolean alive;
+  boolean passedOut;
 
   public Pirate() {
     rumLevel = 0;
     alive = true;
+    passedOut = false;
   }
 
   public int drinkSomeRum() {
-    rumLevel++;
-    System.out.println("Zzz...");
+    if (alive) {
+      rumLevel++;
+      System.out.println("Yaharr!");
+    } else {
+      rumLevel = 0;
+      System.out.println("This guy's dead.");
+    }
     return rumLevel;
+  }
+
+  public boolean howsItGoingMate() {
+    if (alive) {
+      if (rumLevel < 5) {
+        System.out.println("Pour me anudder!");
+        passedOut = false;
+      } else {
+        System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+        passedOut = true;
+      }
+    } else {
+      passedOut = false;
+      System.out.println("This guy's dead.");
+    }
+    return passedOut;
+  }
+
+  public boolean die() {
+    return alive = false;
+  }
+
+  public boolean brawl(Pirate pirate) {
+    if (alive) {
+      if (pirate.alive) {
+        Random random = new Random();
+        int brawRoll = random.nextInt(3);
+      } else {
+        System.out.println("The chosen fellow is dead. Why can't you let him rest in pieces?");
+      }
+    } else {
+      System.out.println("This fellow cannot fight. Let him rest in peace.");
+    }
+
   }
 }
