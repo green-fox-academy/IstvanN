e15f2af90561e0abec380b6d998db366c6c2b3ee
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-20 12:55:24 +0200

    init Doubled

---
+++ week-03/day-02/src/Doubled.java
@@ -0,0 +1,7 @@
+public class Doubled {
+  public static void main(String[] args) {
+
+  }
+
+  public static 
+}
--- week-03/day-02/src/MachineMain.java
+++
@@ -1,9 +0,0 @@
-public class MachineMain {
-  public static void main(String[] args) {
-    Machine machine = new Machine();
-    Machine.greeter("Archie");
-
-    machine.noGreeter("Istvan");
-    System.out.println(machine.age);
-  }
-}
