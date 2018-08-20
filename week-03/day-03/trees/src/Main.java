160ba1b42b8d09d214df5d6b53ca56d5557fdb46
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-20 12:48:50 +0200

    redid doable homework with classes

---
+++ week-03/day-03/trees/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    Tree oak = new Tree("Oak", "Yellow,", 50, true);
+    // and so on...
+  }
+}
---
+++ week-03/day-03/trees/src/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+  String type;
+  String leafColor;
+  int age;
+  boolean isMale;
+
+  public Tree(String type, String leafColor, int age, boolean isMale) {
+    this.type = type;
+    this.leafColor = leafColor;
+    this.age = age;
+    this.isMale = isMale;
+  }
+}
