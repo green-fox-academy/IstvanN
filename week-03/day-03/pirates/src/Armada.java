88aba7b74916d18131549c2d8663bdd540e07ee7
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-20 11:41:51 +0200

    testing WarApp

--- week-03/day-03/pirates/src/Armada.java
+++ week-03/day-03/pirates/src/Armada.java
@@ -1,3 +1,5 @@
+import jdk.management.resource.internal.inst.AbstractInterruptibleChannelRMHooks;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -29,11 +31,35 @@ public class Armada {
     return armada.size();
   }
 
-  public boolean war(Armada otherArmada) {
+  public List remove(int index) {
+    armada.remove(index);
+    return armada;
+  }
+
+  public List shipBattle(Armada otherArmada) {
     for (int i = 0; i < armada.size(); i++) {
       for (int j = 0; j < otherArmada.size(); j++) {
+        System.out.println("Ship " + (i + 1) + "(score: " + armada.get(i).score + ") vs. Ship " + (j + 1) + "(score: " + otherArmada.getShip(j).score + ")");
         if (armada.get(i).score > otherArmada.getShip(j).score) {
+          otherArmada.remove(j);
+        } else {
+          armada.remove(i);
+        }
       }
+    }
+    return armada;
+  }
+
+  public void war(Armada otherArmada) {
+    while (true) {
+      if (armada.size() == 0) {
+        System.out.println("The winner of the war is: " + otherArmada.name);
+        break;
+      } else if (otherArmada.size() == 0) {
+        System.out.println("The winner of the war is: " + name);
+        break;
+      }
+      shipBattle(otherArmada);
     }
   }
 }
---
+++ week-03/day-03/pirates/src/WarApp.java
@@ -0,0 +1,11 @@
+public class WarApp {
+  public static void main(String[] args) {
+    Armada pirates = new Armada("Jolly Rogers");
+    pirates.fillArmada();
+    Armada royal = new Armada("Royal Army");
+    royal.fillArmada();
+    System.out.println(pirates.size());
+    System.out.println(royal.size());
+    pirates.war(royal);
+  }
+}
