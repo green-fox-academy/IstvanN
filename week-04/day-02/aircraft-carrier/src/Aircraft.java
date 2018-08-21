e7937afdc0fbe34349c0039fea31604f23d207d1
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-21 16:42:58 +0200

    created methods for carrier class

--- week-04/day-02/aircraft-carrier/src/Aircraft.java
+++ week-04/day-02/aircraft-carrier/src/Aircraft.java
@@ -25,7 +25,6 @@ public class Aircraft {
       currentAmmo = maxAmmo;
       remainingAmmo = ammoToBeAdded - maxAmmo;
     }
-
     return remainingAmmo;
   }
 
--- week-04/day-02/aircraft-carrier/src/Carrier.java
+++ week-04/day-02/aircraft-carrier/src/Carrier.java
@@ -25,4 +25,52 @@ public class Carrier {
     }
     return ammoNeeded;
   }
+
+  public void fillWithPriority() {
+    for (Aircraft aircraft : aircrafts) {
+      if (aircraft.isPriority()) {
+        ammoStorage = aircraft.refill(ammoStorage);
+      }
+    }
+    if (ammoStorage != 0) {
+      for (Aircraft aircraft : aircrafts) {
+        if (!aircraft.isPriority()) {
+          ammoStorage = aircraft.refill(ammoStorage);
+        }
+      }
+    }
+  }
+
+  public void fillAllAircrafts() {
+    if (ammoStorage == 0) {
+      System.out.println("The ammo storage is empty Sir, can't do shit.");
+    } else if (ammoStorage >= calculateAmmoNeeded()) {
+      for (Aircraft aircraft : aircrafts) {
+        ammoStorage = aircraft.refill(ammoStorage);
+      }
+    } else {
+      fillWithPriority();
+    }
+  }
+
+  public int calculateMaxDamage() {
+    int maxDamage = 0;
+    for (Aircraft aircraft : aircrafts) {
+      maxDamage += aircraft.maxDamage;
+    }
+    return maxDamage;
+  }
+
+  public String getStatus() {
+    String status = "HP: " + healthPoint + ", Aircraft count: " + aircrafts.size() + ", Ammo storage: " + ammoStorage + ", Total damage: " + calculateMaxDamage() + "\n"
+        + "Aircrafts:\n";
+    if (healthPoint > 0) {
+      for (Aircraft aircraft : aircrafts) {
+        status += aircraft.getStatus() + "\n";
+      }
+    } else {
+      status = "It's dead, Jim! :(";
+    }
+    return status;
+  }
 }
