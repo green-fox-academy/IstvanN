101d97aee289dd5924d377626975b9211358fd75
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-21 15:06:19 +0200

    updated Aircraft class with flight() and refill() methods

--- week-04/day-02/aircraft-carrier/src/Aircraft.java
+++ week-04/day-02/aircraft-carrier/src/Aircraft.java
@@ -6,4 +6,23 @@ public class Aircraft {
   public Aircraft() {
     currentAmmo = 0;
   }
+
+  public int flight() {
+    int damageDone = currentAmmo * damage;
+    currentAmmo = 0;
+    return damageDone;
+  }
+
+  public int refill(int ammoToBeAdded) {
+    int remainingAmmo;
+    if (ammoToBeAdded < maxAmmo) {
+      currentAmmo += ammoToBeAdded;
+      remainingAmmo = 0;
+    } else {
+      currentAmmo = maxAmmo;
+      remainingAmmo = ammoToBeAdded - maxAmmo;
+    }
+
+    return remainingAmmo;
+  }
 }
