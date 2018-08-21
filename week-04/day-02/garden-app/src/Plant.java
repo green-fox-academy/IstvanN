dcdac0404cfecf33c88385e5c44ac4e536977f15
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-21 13:39:45 +0200

    init Flower class

--- week-04/day-02/garden-app/src/Plant.java
+++ week-04/day-02/garden-app/src/Plant.java
@@ -1,10 +1,6 @@
 public class Plant {
   String color;
   double waterAmount;
-  int waterneed;
-
-  public Plant(String color, double waterAmount) {
-    this.color = color;
-    this.waterAmount = waterAmount;
-  }
+  int waterNeed;
+  double waterAbsorb;
 }
