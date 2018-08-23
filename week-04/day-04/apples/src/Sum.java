bc543799b3dbb503e80f2f158788dd0f7c0fca18
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-08-23 14:37:23 +0200

    Sum creat method tested

--- week-04/day-04/apples/src/Sum.java
+++ week-04/day-04/apples/src/Sum.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sum {
   List<Integer> list;
 
-
+  public Sum() {
+    list = new ArrayList<>();
+  }
+  public int getSize() {
+    return list.size();
+  }
 }
--- week-04/day-04/apples/test/SumTest.java
+++ week-04/day-04/apples/test/SumTest.java
@@ -1,5 +1,19 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SumTest {
-  
+  Sum l;
+
+  @Before
+  public void before() {
+    l = new Sum();
+  }
+
+  @Test
+  public void createEmptyList() {
+    Assert.assertEquals(0, l.getSize());
+  }
 }
