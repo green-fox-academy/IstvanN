964770129700522fededb0b3f313778e118eb94f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-10 14:49:48 +0200

    added e7

--- week-06/day-01/src/Exercise7.java
+++ week-06/day-01/src/Exercise7.java
@@ -1,11 +1,14 @@
-import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Exercise7 {
   public static void main(String[] args) {
-
+    System.out.println(findFrequenciesOfCharsIn("wowenwilson"));
   }
 
-  public static HashMap<Character, Integer> findFrequenciesOfCharsIn(String input) {
-    
+  public static Map<Character, Long> findFrequenciesOfCharsIn(String input) {
+    return input.chars()
+        .mapToObj(c -> (char) c)
+        .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
   }
 }
