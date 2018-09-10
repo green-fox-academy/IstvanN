3ed1a0c16e4a542fd1ae78d4f1388528d240224a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-10 13:57:33 +0200

    added e5

--- week-06/day-01/src/Exercise5.java
+++ week-06/day-01/src/Exercise5.java
@@ -1,5 +1,13 @@
 public class Exercise5 {
   public static void main(String[] args) {
-    
+    String upperCaseLetters = "World Of Warcraft";
+    collectUppercaseCharacters(upperCaseLetters);
+  }
+
+  public static void collectUppercaseCharacters(String input) {
+    input.chars()
+        .filter(c -> c >= 65 && c <= 90)
+        .mapToObj(c -> Character.toString((char) c))
+        .forEach(System.out::print);
   }
 }
