be7ceaed705180cf36c596084dde5e9873a4ea62
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-10 14:08:58 +0200

    init e7

---
+++ week-06/day-01/src/Exercise7.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Exercise7 {
+  public static void main(String[] args) {
+
+  }
+
+  public static HashMap<Character, Integer> findFrequenciesOfCharsIn(String input) {
+    
+  }
+}
