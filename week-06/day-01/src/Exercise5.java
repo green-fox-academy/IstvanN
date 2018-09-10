39df92bb81ea326317a99dff11d52e2d3f4d86cf
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-10 14:01:35 +0200

    updated e5 to return a List of Characters

--- week-06/day-01/src/Exercise5.java
+++ week-06/day-01/src/Exercise5.java
@@ -1,13 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Exercise5 {
   public static void main(String[] args) {
     String upperCaseLetters = "World Of Warcraft";
-    collectUppercaseCharacters(upperCaseLetters);
+    List<Character> wow = collectUppercaseCharacters(upperCaseLetters);
+    System.out.println(wow);
   }
 
-  public static void collectUppercaseCharacters(String input) {
-    input.chars()
+  public static List<Character> collectUppercaseCharacters(String input) {
+    return input.chars()
         .filter(c -> c >= 65 && c <= 90)
-        .mapToObj(c -> Character.toString((char) c))
-        .forEach(System.out::print);
+        .mapToObj(c -> Character.valueOf((char) c))
+        .collect(Collectors.toList());
   }
 }
