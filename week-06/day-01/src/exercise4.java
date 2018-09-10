d0c8fff364f0625410648b14b442c7c4f94f3d8f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-10 13:28:07 +0200

    added e4

--- week-06/day-01/src/exercise4.java
+++ week-06/day-01/src/exercise4.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class exercise4 {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
 
-    
+    List<Integer> numbersThatHasSquaredValuesMoreThan20 = numbers
+        .stream()
+        .filter(n -> n * n > 20)
+        .collect(Collectors.toList());
+
+    System.out.println(numbersThatHasSquaredValuesMoreThan20);
   }
 }
