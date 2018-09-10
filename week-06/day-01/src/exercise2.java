28571555c09c845bc8cc8d80b9b0057c6360c9f9
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-10 13:12:57 +0200

    added e2

--- week-06/day-01/src/exercise2.java
+++ week-06/day-01/src/exercise2.java
@@ -5,6 +5,13 @@ public class exercise2 {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
-    
+    double average = numbers
+        .stream()
+        .filter(n -> n % 2 != 0)
+        .mapToInt(n -> n)
+        .average()
+        .getAsDouble();
+
+    System.out.println(average);
   }
 }
