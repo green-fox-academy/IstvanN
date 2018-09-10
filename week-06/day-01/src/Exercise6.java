aa08000816a4e49d293601de16ba777627373613
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-10 14:05:16 +0200

    added e6

--- week-06/day-01/src/Exercise6.java
+++ week-06/day-01/src/Exercise6.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise6 {
   public static void main(String[] args) {
     ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+
+    List<String> citiesStartsWithAEndsWithI = cities
+        .stream()
+        .filter(s -> s.startsWith("A") && s.endsWith("I"))
+        .collect(Collectors.toList());
+
+    System.out.println(citiesStartsWithAEndsWithI);
   }
 }
