039c586660222eb064b03b9da9aaa52397dccc06
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-17 13:55:22 +0200

    init Greeting class

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.springstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,7 +15,7 @@ public class SpringstartApplication {
     SpringApplication.run(SpringstartApplication.class, args);
   }
 
-  @RequestMapping(value = "/hello")
+  @GetMapping("/hello")
   @ResponseBody
   public String hello() {
     return "Hello World!";
