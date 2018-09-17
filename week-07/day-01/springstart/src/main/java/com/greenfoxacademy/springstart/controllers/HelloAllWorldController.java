44ce60551026702fb333e5ca7dfc66ba50b36a09
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-17 18:17:27 +0200

    started to add random colors

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllWorldController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllWorldController.java
@@ -5,14 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
+
 @Controller
 public class HelloAllWorldController {
 
   @GetMapping("/randomhello")
   public String greetRandomly(Model model, @RequestParam("name") String name) {
+    Random random = new Random();
+    int randomRGB = random.nextInt(256);
     RandomHello randomHello = new RandomHello();
     model.addAttribute("name", name.toUpperCase());
-    model.addAttribute("randomhello", randomHello.getActualHello());
+    model.addAttribute("randomhello", randomHello.getActualHello().toUpperCase());
+    model.addAttribute("rgb", randomRGB + ", " + randomRGB + ", " + randomRGB);
     return "randomhello";
   }
 }
