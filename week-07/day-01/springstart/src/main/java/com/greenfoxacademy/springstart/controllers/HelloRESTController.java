0b3207423adcf7fc7bd46adf50a202bbcf216e77
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-17 15:26:44 +0200

    done ex with AtomicLong

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  AtomicLong generator = new AtomicLong(1);
 
   @GetMapping("/greeting")
   public Greeting greet(@RequestParam("name") String name) {
-    Greeting greeting = new Greeting(1, String.format("Hello, dear %s!", name.toUpperCase()));
+    long id = generator.incrementAndGet();
+    Greeting greeting = new Greeting(id, String.format("Hello, dear %s!", name.toUpperCase()));
     return greeting;
   }
 }
