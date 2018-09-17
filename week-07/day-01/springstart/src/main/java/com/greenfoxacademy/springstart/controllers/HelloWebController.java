7a77aa0972dce92c3cd7e809e25da1cab2937f17
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-17 17:04:53 +0200

    impemented serverSetupTime to both HTML file and HelloWebController

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloWebController {
+  AtomicLong atomicLongGenerator = new AtomicLong(1);
 
   @GetMapping("/web/greeting")
   public String greeting(Model model) {
+    long serverSetupTime = atomicLongGenerator.incrementAndGet();
     model.addAttribute("name", " World");
+    model.addAttribute("serverSetupTime", serverSetupTime);
     return "greeting";
   }
 }
