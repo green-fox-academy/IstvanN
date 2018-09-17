465ec0cefc8728fbafcde80ea610881f17bf707a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-17 14:46:21 +0200

    init RequestParam method

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,15 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @GetMapping("/greeting")
-  public Greeting greet() {
-    Greeting greeting = new Greeting(1, "Hello World!");
+  public Greeting greet(@RequestParam("name") String name) {
+    Greeting greeting = new Greeting(1, String.format("Hello %s!", name));
     return greeting;
   }
 }
