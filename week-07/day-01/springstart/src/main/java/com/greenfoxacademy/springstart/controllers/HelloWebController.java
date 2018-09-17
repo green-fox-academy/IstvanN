703a7abce4935c0e3299f5c59c42b1904509f18c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-17 17:06:58 +0200

    updated WebContr with greeting with name method

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -11,9 +12,9 @@ public class HelloWebController {
   AtomicLong atomicLongGenerator = new AtomicLong(1);
 
   @GetMapping("/web/greeting")
-  public String greeting(Model model) {
+  public String greeting(Model model, @RequestParam("name") String name) {
     long serverSetupTime = atomicLongGenerator.incrementAndGet();
-    model.addAttribute("name", " World");
+    model.addAttribute("name", name);
     model.addAttribute("serverSetupTime", serverSetupTime);
     return "greeting";
   }
