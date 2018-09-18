4b870938c90090dc6da72ced970166f1bc7a1ea1
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-18 13:50:55 +0200

    remade greeting.html

--- week-07/day-02/submission/src/main/java/com/formsubmission/submission/controllers/GreetingController.java
+++ week-07/day-02/submission/src/main/java/com/formsubmission/submission/controllers/GreetingController.java
@@ -4,6 +4,8 @@ import com.formsubmission.submission.models.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GreetingController {
@@ -12,5 +14,10 @@ public class GreetingController {
   public String showGreeting(Model model) {
     model.addAttribute("greeting", new Greeting());
     return "greeting";
+  }
+
+  @PostMapping("/greeting")
+  public String submitGreeting(@ModelAttribute Greeting greeting) {
+    return "result";
   }
 }
