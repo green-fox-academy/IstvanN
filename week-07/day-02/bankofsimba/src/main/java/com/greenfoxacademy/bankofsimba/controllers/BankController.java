b9d2b4b9cf1f576417225053bf54542373b5370b
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-18 16:39:08 +0200

    init new EndPoint /accounts in controller

--- week-07/day-02/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
+++ week-07/day-02/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class BankController {
 
@@ -19,5 +22,16 @@ public class BankController {
     String enjoy = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
     model.addAttribute("enjoy", enjoy);
     return "ception";
+  }
+
+  @GetMapping("/accounts")
+  public String showListOfAccounts(Model model) {
+    List<BankAccount> accounts = Arrays.asList(
+        new BankAccount("Simba", 2000, "lion", "Zebra"),
+        new BankAccount("Mufasa", 10000, "lion", "Zebra"),
+        new BankAccount("Zordon", 3, "lion", "Hyena"),
+        new BankAccount("Timon", 5, "meerkat", "Bug"),
+        new BankAccount("Pumbaa", 15, "warthog", "Bug")
+    );
   }
 }
