2ff3ed846c46df5532432713f3d967e24240132c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-20 17:29:04 +0200

    localhost is working properly

--- week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/greenfoxclub/controllers/MainController.java
+++ week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/greenfoxclub/controllers/MainController.java
@@ -1,8 +1,13 @@
 package com.greenfoxacademy.greenfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
 }
