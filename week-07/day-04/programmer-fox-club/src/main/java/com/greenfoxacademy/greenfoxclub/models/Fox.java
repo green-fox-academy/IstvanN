71fbffd1a68d6765598b359c8a9be776071cfec2
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-21 11:14:20 +0200

    init Fox class with basic fields, contructors, getters and setters

---
+++ week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/greenfoxclub/models/Fox.java
@@ -0,0 +1,53 @@
+package com.greenfoxacademy.greenfoxclub.models;
+
+import java.util.List;
+
+public class Fox {
+
+  String name;
+  List<String> tricks;
+  String food;
+  String drink;
+
+  public Fox() {
+  }
+
+  public Fox(String name, List<String> tricks, String food, String drink) {
+    this.name = name;
+    this.tricks = tricks;
+    this.food = food;
+    this.drink = drink;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<String> getTricks() {
+    return tricks;
+  }
+
+  public void setTricks(List<String> tricks) {
+    this.tricks = tricks;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
+}
