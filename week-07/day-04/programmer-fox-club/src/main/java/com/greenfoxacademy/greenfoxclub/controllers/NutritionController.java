64d9880948d47f5375ca2f0821cdfe3e999c996a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-22 20:16:59 +0200

    setting food and drink on Nutrition store now working properly

--- week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/greenfoxclub/controllers/NutritionController.java
+++ week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/greenfoxclub/controllers/NutritionController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -27,6 +29,11 @@ public class NutritionController {
     model.addAttribute("drinks", drinks);
 
     return "nutrition-store";
+  }
+
+  @PostMapping("/nutritionStore")
+  public String setFoxNewFoodAndDrink(@ModelAttribute Fox fox) {
+    return "index";
   }
 
 }
