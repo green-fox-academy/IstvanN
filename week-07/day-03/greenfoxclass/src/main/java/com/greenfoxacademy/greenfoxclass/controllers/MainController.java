fc482fce80484ad06d105d9c177f9e1bef37beda
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-23 18:52:32 +0200

    added /save endpoint to save students

--- week-07/day-03/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/controllers/MainController.java
+++ week-07/day-03/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/controllers/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,5 +31,11 @@ public class MainController {
   @GetMapping("/gfa/add")
   public String showAddForm() {
     return "add-student-form";
+  }
+
+  @PostMapping("/gfa/save")
+  public String addNewStudent(@RequestParam("name") String student) {
+    studentService.save(student);
+    return "redirect:/gfa";
   }
 }
