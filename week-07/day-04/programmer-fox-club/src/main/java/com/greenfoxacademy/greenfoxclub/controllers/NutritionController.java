5bbeffa3df01e32255faf7b4cab130adbb0f7e6f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-23 20:28:51 +0200

    nutrition finally working with request params

--- week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/greenfoxclub/controllers/NutritionController.java
+++ week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/greenfoxclub/controllers/NutritionController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -33,13 +32,14 @@ public class NutritionController {
   }
 
   @PostMapping("/nutritionStore")
-  public String setFoxNewFoodAndDrink(@RequestParam("name") String foxName,
+  public String setFoxNewFoodAndDrink(@RequestParam("name") String petName,
                                       @RequestParam("food") String food,
                                       @RequestParam("drink") String drink,
                                       RedirectAttributes redir) {
-    foxService.getFoxByName(foxName).setFood(food);
-    foxService.getFoxByName(foxName).setDrink(drink);
-    redir.addAttribute("name", foxName);
+    Fox fox = foxService.getFoxByName(petName);
+    fox.setFood(food);
+    fox.setDrink(drink);
+    redir.addAttribute("name", petName);
     return "redirect:/";
   }
 
