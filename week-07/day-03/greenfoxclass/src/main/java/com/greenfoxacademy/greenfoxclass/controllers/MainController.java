0bedf99f9714b3d0828f3e412ce339da8930a5eb
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-23 18:36:10 +0200

    init StudentService

--- week-07/day-03/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/controllers/MainController.java
+++ week-07/day-03/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/controllers/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
   public String index() {
     return "index";
   }
+
+  @GetMapping("/gfa/list")
+  public String showStudentsList() {
+    return "students";
+  }
 }
