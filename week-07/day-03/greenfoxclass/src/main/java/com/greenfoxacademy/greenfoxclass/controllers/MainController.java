3ca5ac2c9b057b1a88a9255465831cd51023bff7
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-23 18:43:39 +0200

    setted up gfa/list endpoint and students.html

--- week-07/day-03/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/controllers/MainController.java
+++ week-07/day-03/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/controllers/MainController.java
@@ -1,10 +1,18 @@
 package com.greenfoxacademy.greenfoxclass.controllers;
 
+import com.greenfoxacademy.greenfoxclass.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+
+  @Autowired
+  StudentService studentService;
 
   @GetMapping("/gfa")
   public String index() {
@@ -12,7 +20,9 @@ public class MainController {
   }
 
   @GetMapping("/gfa/list")
-  public String showStudentsList() {
+  public String showStudentsList(Model model) {
+    List<String> students = studentService.findAll();
+    model.addAttribute("students", students);
     return "students";
   }
 }
