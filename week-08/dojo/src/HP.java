c682497785373e1438212681f4bd0f04aaae0c14
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-24 10:54:44 +0200

    Books are not  8. bad

--- week-08/dojo/src/HP.java
+++ week-08/dojo/src/HP.java
@@ -1,15 +1,3 @@
 public class HP {
-  int book1;
-  int book2;
-  int book3;
-  int book4;
-  int book5;
 
-  public HP {
-    book1 = 8;
-    book2 = 8;
-    book3 = 8;
-    book4 = 8;
-    book5 = 8;
-  }
 }
--- week-08/dojo/test/HPTest.java
+++ week-08/dojo/test/HPTest.java
@@ -1,5 +1,17 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class HPTest {
+
+  @Test
+  public void testWithZeroBooks() {
+    List<Integer> books = new ArrayList<>();
+    assertEquals(0, HP.getPrice(books));
+  }
 
 }
