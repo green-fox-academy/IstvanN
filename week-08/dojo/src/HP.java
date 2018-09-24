3b9eb5f57a7fd1ffb7a61e7ca7f0f98ddcef52dc
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-24 11:22:23 +0200

    Books are good. yes

--- week-08/dojo/src/HP.java
+++ week-08/dojo/src/HP.java
@@ -1,3 +1,18 @@
+import java.util.List;
+
 public class HP {
 
+  public static double getPrice(List<Integer> books) {
+    if (books.size() == 0) {
+      return 0;
+    }
+
+    if (books.size() == 2) {
+      if (books.get(0) == books.get(1)) {
+        return 16;
+      } else return 15.2;
+    }
+
+    return 8;
+  }
 }
--- week-08/dojo/test/HPTest.java
+++ week-08/dojo/test/HPTest.java
@@ -1,17 +1,82 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class HPTest {
 
+  List<Integer> books;
+  double price;
+
+  @Before
+  public void before() {
+    books = new ArrayList<>();
+    price = 8;
+  }
+
   @Test
   public void testWithZeroBooks() {
-    List<Integer> books = new ArrayList<>();
-    assertEquals(0, HP.getPrice(books));
+    assertEquals(0, HP.getPrice(books), 0);
   }
+
+  @Test
+  public void testWithOneBook() {
+    books.add(1);
+    assertEquals(price, HP.getPrice(books), 0);
+  }
+
+  @Test
+  public void testWithTwoOfTheSameBook() {
+    books.add(1);
+    books.add(1);
+    assertEquals(price * 2, HP.getPrice(books), 0);
+  }
+
+  @Test
+  public void testWithThreeOfTheSameBook() {
+    books.add(1);
+    books.add(1);
+    books.add(1);
+    assertEquals(price * 3, HP.getPrice(books), 0);
+  }
+
+  @Test
+  public void testWithFourOfTheSameBook() {
+    books.add(1);
+    books.add(1);
+    books.add(1);
+    books.add(1);
+    assertEquals(price * 4, HP.getPrice(books), 0);
+  }
+
+  @Test
+  public void testWithFiveOfTheSameBook() {
+    books.add(1);
+    books.add(1);
+    books.add(1);
+    books.add(1);
+    books.add(1);
+    assertEquals(price * 5, HP.getPrice(books), 0);
+  }
+
+  @Test
+  public void testWithDifferentBooks() {
+    books.add(1);
+    books.add(2);
+    double expected = price * 2 * 0.95;
+    assertEquals(expected, HP.getPrice(books), 0);
+  }
+
+
+//
+//  @Test
+//  public void testWhenContainsWrongBook() {
+//    List<Integer> books = new ArrayList<>();
+//    books.add(1);
+//    assertEquals(8, HP.getPrice(books));
+//  }
 
 }
