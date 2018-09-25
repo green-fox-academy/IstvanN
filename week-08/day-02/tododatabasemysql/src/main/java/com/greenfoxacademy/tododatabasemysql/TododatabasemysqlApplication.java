fcf90526b9922be1a77c109e9cc7cda1b682c45c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-25 14:13:01 +0200

    feat(todoslist.html): added tableform for the html

--- week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/TododatabasemysqlApplication.java
+++ week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/TododatabasemysqlApplication.java
@@ -1,16 +1,13 @@
 package com.greenfoxacademy.tododatabasemysql;
 
-import com.greenfoxacademy.tododatabasemysql.models.Todo;
 import com.greenfoxacademy.tododatabasemysql.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
 
 @SpringBootApplication
-public class TododatabasemysqlApplication implements CommandLineRunner {
+public class TododatabasemysqlApplication {
 
   @Autowired
   TodoRepository todoRepository;
@@ -19,12 +16,4 @@ public class TododatabasemysqlApplication implements CommandLineRunner {
     SpringApplication.run(TododatabasemysqlApplication.class, args);
   }
 
-  @Override
-  public void run(String... args) throws Exception {
-    Todo todo1 = new Todo("I have to learn to use DBs");
-    todoRepository.save(todo1);
-    Todo todo2 = new Todo("I have to play some Sword Coast Legends");
-    todoRepository.save(todo2);
-    todoRepository.saveAll(Arrays.asList(new Todo("Lol"), new Todo("Lel")));
-  }
 }
