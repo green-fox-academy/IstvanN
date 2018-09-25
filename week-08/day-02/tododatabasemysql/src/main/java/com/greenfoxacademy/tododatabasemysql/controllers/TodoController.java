1188f56c7ffa770e85fc67a45e888046c2c28315
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-25 13:30:31 +0200

    feat(controller): injected repo into controller and made the first get method

--- week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/controllers/TodoController.java
+++ week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/controllers/TodoController.java
@@ -1,4 +1,21 @@
 package com.greenfoxacademy.tododatabasemysql.controllers;
 
+import com.greenfoxacademy.tododatabasemysql.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
 public class TodoController {
+
+  @Autowired
+  TodoRepository todoRepository;
+
+  @GetMapping(value = {"", "/list"})
+  public String list(Model model) {
+
+  }
 }
--- week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/repositories/TodoRepository.java
+++ week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/repositories/TodoRepository.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.tododatabasemysql.repositories;
 
 import com.greenfoxacademy.tododatabasemysql.models.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
