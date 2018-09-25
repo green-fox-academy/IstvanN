e59ce2172d348c48bc9493a651747b094fd03fe9
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-25 12:22:42 +0200

    init Todo with h2 database exercise

---
+++ week-08/day-02/tododatabaseh2/src/main/java/com/greenfoxacademy/tododatabaseh2/Tododatabaseh2Application.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.tododatabaseh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tododatabaseh2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Tododatabaseh2Application.class, args);
+  }
+}
