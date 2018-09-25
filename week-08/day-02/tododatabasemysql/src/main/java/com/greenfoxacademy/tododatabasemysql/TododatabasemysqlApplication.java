2596afcac117b9a777009163a8b906c6c3a7d297
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-25 14:17:29 +0200

    style(html): styled html's table

--- week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/TododatabasemysqlApplication.java
+++ week-08/day-02/tododatabasemysql/src/main/java/com/greenfoxacademy/tododatabasemysql/TododatabasemysqlApplication.java
@@ -1,7 +1,5 @@
 package com.greenfoxacademy.tododatabasemysql;
 
-import com.greenfoxacademy.tododatabasemysql.repositories.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TododatabasemysqlApplication {
 
-  @Autowired
-  TodoRepository todoRepository;
-
   public static void main(String[] args) {
     SpringApplication.run(TododatabasemysqlApplication.class, args);
   }
-
 }
