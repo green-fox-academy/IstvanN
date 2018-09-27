690efea9e88114ba11b02b6232b73cee164ab3df
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-27 16:06:02 +0200

    feat(index.html): connected the index to the list of games, and inserted in table

--- week-08/day-04/need-for-sequel/src/main/java/com/greenfoxacademy/needforsequel/controllers/MainController.java
+++ week-08/day-04/need-for-sequel/src/main/java/com/greenfoxacademy/needforsequel/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.needforsequel.controllers;
 import com.greenfoxacademy.needforsequel.services.VideoGameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,7 +14,8 @@ public class MainController {
   private VideoGameService videoGameService;
 
   @GetMapping("/")
-  public String showIndex() {
+  public String showIndex(Model model) {
+    model.addAttribute("games", videoGameService.getVideoGames());
     return "index";
   }
 
--- week-08/day-04/need-for-sequel/src/main/java/com/greenfoxacademy/needforsequel/models/VideoGame.java
+++ week-08/day-04/need-for-sequel/src/main/java/com/greenfoxacademy/needforsequel/models/VideoGame.java
@@ -1,16 +1,11 @@
 package com.greenfoxacademy.needforsequel.models;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
 public class VideoGame {
 
   @Id
@@ -29,6 +24,46 @@ public class VideoGame {
     this.title = title;
     this.company = company;
     this.income = income;
+    this.age = age;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  public int getIncome() {
+    return income;
+  }
+
+  public void setIncome(int income) {
+    this.income = income;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
     this.age = age;
   }
 }
