4081a65fd1ca27872354ef1d7f8d49ef36ca10d2
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-28 13:10:30 +0200

    style(index.html): styled with bootstrap

--- week-08/day-05/redditlike/src/main/java/com/greenfoxacademy/redditlike/controllers/PostController.java
+++ week-08/day-05/redditlike/src/main/java/com/greenfoxacademy/redditlike/controllers/PostController.java
@@ -1,13 +1,20 @@
 package com.greenfoxacademy.redditlike.controllers;
 
+import com.greenfoxacademy.redditlike.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostController {
 
+  @Autowired
+  PostService postService;
+
   @GetMapping("/")
-  public String showMain() {
+  public String showMain(Model model) {
+    model.addAttribute("posts", postService.getPosts());
     return "index";
   }
 }
