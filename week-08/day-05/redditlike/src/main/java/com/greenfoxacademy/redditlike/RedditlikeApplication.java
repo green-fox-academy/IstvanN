ac6140d5cee9d94b50ec0ab11bb4868912b84099
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-28 11:11:26 +0200

    chore(app): init application

---
+++ week-08/day-05/redditlike/src/main/java/com/greenfoxacademy/redditlike/RedditlikeApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.redditlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditlikeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedditlikeApplication.class, args);
+  }
+}
