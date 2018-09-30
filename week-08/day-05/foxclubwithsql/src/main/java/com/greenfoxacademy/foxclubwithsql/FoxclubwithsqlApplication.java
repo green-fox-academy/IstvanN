48d1938515d8da9c8193f67ffb6d7b359b20c6f6
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-09-30 16:34:36 +0200

    chore(foxclub app): init the application

---
+++ week-08/day-05/foxclubwithsql/src/main/java/com/greenfoxacademy/foxclubwithsql/FoxclubwithsqlApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.foxclubwithsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxclubwithsqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoxclubwithsqlApplication.class, args);
+  }
+}
---
+++ week-08/day-05/foxclubwithsql/src/test/java/com/greenfoxacademy/foxclubwithsql/FoxclubwithsqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.foxclubwithsql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoxclubwithsqlApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
