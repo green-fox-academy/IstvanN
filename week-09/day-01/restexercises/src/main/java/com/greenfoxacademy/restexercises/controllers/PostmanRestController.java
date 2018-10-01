45060772c8daed8adc8f50c3ece60e130a34a98e
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-10-01 15:53:49 +0200

    feat(controller): create /arrays endpoint with logic

--- week-09/day-01/restexercises/src/main/java/com/greenfoxacademy/restexercises/controllers/PostmanRestController.java
+++ week-09/day-01/restexercises/src/main/java/com/greenfoxacademy/restexercises/controllers/PostmanRestController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.restexercises.controllers;
 
+import com.greenfoxacademy.restexercises.models.postman.arrayhandler.ArrayHandler;
 import com.greenfoxacademy.restexercises.models.postman.arrayhandler.WhatNumbers;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,8 @@ public class PostmanRestController {
 
   @PostMapping("/arrays")
   public Object handleArray(@RequestBody(required = false) WhatNumbers whatNumbers) {
-
+    ArrayHandler arrayHandler = new ArrayHandler();
+    arrayHandler.handleWhatNumber(whatNumbers);
+    return arrayHandler;
   }
 }
