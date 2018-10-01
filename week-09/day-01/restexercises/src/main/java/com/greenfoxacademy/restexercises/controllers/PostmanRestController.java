6909a2482b8e8311a692c98ed0b02cdeaacf76f4
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-10-01 15:18:48 +0200

    chore(PostmanController): init new controller

---
+++ week-09/day-01/restexercises/src/main/java/com/greenfoxacademy/restexercises/controllers/PostmanRestController.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.restexercises.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostmanRestController {
+
+  @PostMapping("/arrays")
+
+}
---
+++ week-09/day-01/restexercises/src/main/java/com/greenfoxacademy/restexercises/models/postman/ArrayHandler.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.restexercises.models.postman;
+
+public class ArrayHandler {
+}
