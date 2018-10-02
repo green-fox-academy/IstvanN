7d2737e498930c16f8a899ef487dbbace60c10fa
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-10-02 13:49:25 +0200

    feat(Nutrition): init nutrition as an entity

---
+++ week-08/day-05/foxclubwithsql/src/main/java/com/greenfoxacademy/foxclubwithsql/models/Nutrition.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.foxclubwithsql.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Nutrition {
+
+  private String type;
+  private String name;
+}
