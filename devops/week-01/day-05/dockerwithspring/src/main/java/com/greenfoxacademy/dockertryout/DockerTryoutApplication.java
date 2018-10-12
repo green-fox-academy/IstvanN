ea1bbfa1a725e424e9fa5cc711d1c77d66e1c21f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-10-12 11:21:18 +0200

    chore(docker tryout app): init tryout app with main controller

---
+++ devops/week-01/day-05/dockerwithspring/src/main/java/com/greenfoxacademy/dockertryout/DockerTryoutApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.dockertryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTryoutApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DockerTryoutApplication.class, args);
+  }
+}
